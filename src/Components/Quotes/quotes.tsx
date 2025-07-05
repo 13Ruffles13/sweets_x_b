72bbbc825bed53029cfdaaae0eca8040574139fb
Rafael Alvarado <87215494+13Ruffles13@users.noreply.github.com>
2025-07-05 12:02:29 -0700

    quotes Component (added) + brightness adjustment to homepage

--- src/Components/Quotes/quotes.tsx
+++ src/Components/Quotes/quotes.tsx
@@ -1,7 +1,24 @@
 import "./quotes.css";
 
 function Quotes() {
-  return <></>;
+  return (
+    <>
+      <div className="quotesMainContainer">
+        <div className="whiteSugarHeart">
+          {" "}
+          <>&#129293;</>
+        </div>
+        <div id="whiteSweetQuote">
+          Baked with the finest natural ingredients. Delivered fresh from the
+          oven. Perfection
+        </div>
+        <div className="whiteSugarHeart">
+          {" "}
+          <>&#129293;</>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Quotes;
