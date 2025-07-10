be7ea814b09b95cd0bf328fa0908c631d9564c66
Rafael Alvarado <87215494+13Ruffles13@users.noreply.github.com>
2025-07-10 14:05:32 -0700

    Offers Component

--- src/Components/SweetOffers/SweetOffers.tsx
+++ src/Components/SweetOffers/SweetOffers.tsx
@@ -1,35 +1,58 @@
-import "./SweetOffers.css"
+import "./SweetOffers.css";
 
-function SweetOffers(){
-    return(
-        <div className="sweetOffersMainContainer">
-            <h1>SWEET OFFERS</h1>
-            <table className="offersTable">
-                <th className="offersTableHeaders">
-                    <tr>
-                        <div className="offersContainerBorder">
-                            <div>Data</div>
-                        </div>
-                    </tr>
-                </th>
-                <th className="offersTableHeaders">
-                    <tr>
-                        <div className="offersContainerBorder">
-                            <div>Data</div>
-                        </div>
-                    </tr>
-                </th>
-                <th className="offersTableHeaders">
-                    <tr>
-                        <div className="offersContainerBorder">
-                            <div>Data</div>
-                        </div>
-                    </tr>
-                </th>
-            </table>
-        </div>
-
-    )
+function SweetOffers() {
+  return (
+    <div className="sweetOffersMainContainer">
+      <h1>SWEET OFFERS</h1>
+      <table className="offersTable">
+        <th className="offersTableHeaders">
+          <tr>
+            <div className="offersContainerBorder">
+              <h1>Sweevory Magazine</h1>
+              <div className="whiteSugarHeart">
+                {" "}
+                <>&#129293;</>
+              </div>
+              <p>
+                News write-ups offer a great way to let clients know about new
+                products and services, events, awards, and more!
+              </p>
+            </div>
+          </tr>
+        </th>
+        <th className="offersTableHeaders">
+          <tr>
+            <div className="offersContainerBorder">
+              <h1>Sweevory Magazine</h1>
+              <div className="whiteSugarHeart">
+                {" "}
+                <>&#129293;</>
+              </div>
+              <p>
+                News write-ups offer a great way to let clients know about new
+                products and services, events, awards, and more!
+              </p>
+            </div>
+          </tr>
+        </th>
+        <th className="offersTableHeaders">
+          <tr>
+            <div className="offersContainerBorder">
+              <h1>Sweevory Magazine</h1>
+              <div className="whiteSugarHeart">
+                {" "}
+                <>&#129293;</>
+              </div>
+              <p>
+                News write-ups offer a great way to let clients know about new
+                products and services, events, awards, and more!
+              </p>
+            </div>
+          </tr>
+        </th>
+      </table>
+    </div>
+  );
 }
 
-export default SweetOffers
+export default SweetOffers;
