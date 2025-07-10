db51642f3d331d04eefc6050a5249571415a19cb
Rafael Alvarado <87215494+13Ruffles13@users.noreply.github.com>
2025-07-10 15:08:33 -0700

    Phase 2 images of sweet options

--- src/Components/SweetOptions/SweetOptions.tsx
+++ src/Components/SweetOptions/SweetOptions.tsx
@@ -1,5 +1,7 @@
 import "./SweetOptions.css";
-import SweetBeignets from "../../assets/Sweet_Beignets.jpg"
+import SweetBeignets from "../../assets/Sweet_Beignets.jpg";
+import SweetBlueBerryMuffins from "../../assets/Sweet_BlueBerryMuffins.jpg";
+import SweetYellowCake from "../../assets/sweetsSugarPowderYellowCake.png";
 
 function SweetOptions() {
   return (
@@ -8,17 +10,26 @@ function SweetOptions() {
       <table className="sweetOptionsTable">
         <th className="sweetImage">
           <tr>
-            <img src={SweetBeignets} alt="Sweet Beignets" />
+            <div>
+              <h2>Beignets</h2>
+              <img src={SweetBeignets} alt="Sweet Beignets" />
+            </div>
           </tr>
         </th>
         <th className="sweetImage">
           <tr>
-            <div className="">Hello</div>
+            <div>
+              <h2>BlueBerry Muffins</h2>
+              <img src={SweetBlueBerryMuffins} alt="Sweet Blueberry Muffins" />
+            </div>
           </tr>
         </th>
         <th className="sweetImage">
           <tr>
-            <div className="">Hello</div>
+            <div>
+              <h2>Yellow Cake</h2>
+              <img src={SweetYellowCake} alt="Sweet Yellow Cake" />
+            </div>
           </tr>
         </th>
       </table>
