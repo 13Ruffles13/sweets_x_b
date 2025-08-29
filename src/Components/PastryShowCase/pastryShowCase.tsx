7c7a48ec66732dd3faa9e2f3f95625e22545c92b
13Ruffles13 <87215494+13Ruffles13@users.noreply.github.com>
2025-08-28 20:56:25 -0700

    Page: pastryShowCase.tsx + .css
    Refactor - removed tables from .tsx filed. Added div containers to
    handle mobile responsiveness in the .css file
    
    @media query max-width: 768px

--- src/Components/PastryShowCase/pastryShowCase.tsx
+++ src/Components/PastryShowCase/pastryShowCase.tsx
@@ -4,34 +4,30 @@ import "./pastryShowCase.css";
 function PastryShowCase() {
   return (
     <div className="mainPastryShowCaseContainer">
-      <table>
-        <th className="showCaseCol1">
-          <div>
-            <div className="whiteSugarHeart">
-              {" "}
-              <>&#129293;</>
-            </div>
-            <h1 id="pastryTitle">
-              PASTRY <br /> PERFECTION
-            </h1>
-            <p className="pastryFeedText">
-              Discover deliciousness at Chervet Bakery. Imagine biting into a
-              freshly baked treat that's made with love and handled with care.
-              <br />
-              <br />
-              No need to imagine, because our pastries are baked, packed, and
-              delivered on the day they're made, so you're assured of oven-fresh
-              goodness.
-              <br /> 
-              <br />
-              Now that's perfection.
-            </p>
-          </div>
-        </th>
-        <th className="showCaseCol2">
-          <img src={yellowCake} alt="Powder Sugar Yellow Cake" />
-        </th>
-      </table>
+      {/* Left side: Text */}
+      <div className="showCaseCol1">
+        <div className="whiteSugarHeart">&#129293;</div>
+        <h1 id="pastryTitle">
+          PASTRY <br /> PERFECTION
+        </h1>
+        <p className="pastryFeedText">
+          Discover deliciousness at Chervet Bakery. Imagine biting into a
+          freshly baked treat that's made with love and handled with care.
+          <br />
+          <br />
+          No need to imagine, because our pastries are baked, packed, and
+          delivered on the day they're made, so you're assured of oven-fresh
+          goodness.
+          <br />
+          <br />
+          Now that's perfection.
+        </p>
+      </div>
+
+      {/* Right side: Image */}
+      <div className="showCaseCol2">
+        <img src={yellowCake} alt="Powder Sugar Yellow Cake" />
+      </div>
     </div>
   );
 }
