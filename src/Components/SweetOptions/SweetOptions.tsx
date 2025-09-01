665c70644cb9e7035d22d87d6ed82cd6ef0eeb1f
13Ruffles13 <87215494+13Ruffles13@users.noreply.github.com>
2025-09-01 12:33:49 -0700

    SweetOptions.tsx/.css
    Refactor - .tsx - Page removing tables elements replaced with div
    elements. Cleaned up class names
    
    Responsive breakpoints - .css - Added grid-tem-col flow to the
    sweetOptionsGrid class. Allowing for mobile responsiveness. Adjusted
    computed props.

--- src/Components/SweetOptions/SweetOptions.tsx
+++ src/Components/SweetOptions/SweetOptions.tsx
@@ -7,41 +7,29 @@ function SweetOptions() {
   return (
     <div className="sweetOptionsMainContainer">
       <h1>What you'll love</h1>
-      <table className="sweetOptionsTable">
-        <th className="sweetImage">
-          <tr>
-            <div className="sweetImageContainer">
-              <div className="sweetImageDetails">
-                <h2>Beignets</h2>
-                <p>Heavenly pastry creations that will satisfy your cravings</p>
-              </div>
-              <img src={SweetBeignets} alt="Sweet Beignets" />
-            </div>
-          </tr>
-        </th>
-        <th className="sweetImage">
-          <tr>
-            <div className="sweetImageContainer">
-              <div className="sweetImageDetails">
-                <h2>Blueberry Muffins</h2>
-                <p>Deliciously decadent cookies that are sure to impress</p>
-              </div>
-              <img src={SweetBlueBerryMuffins} alt="Sweet Blueberry Muffins" />
-            </div>
-          </tr>
-        </th>
-        <th className="sweetImage">
-          <tr>
-            <div className="sweetImageContainer">
-              <div className="sweetImageDetails">
-                <h2>Yellow Cake</h2>
-                <p>Freshly-baked loaves of bread you'll find irresistible</p>
-              </div>
-              <img src={SweetYellowCake} alt="Sweet Yellow Cake" />
-            </div>
-          </tr>
-        </th>
-      </table>
+      <div className="sweetOptionsGrid">
+        <div className="sweetImageContainer">
+          <div className="sweetImageDetails">
+            <h2>Beignets</h2>
+            <p>Heavenly pastry creations that will satisfy your cravings</p>
+          </div>
+          <img src={SweetBeignets} alt="Sweet Beignets" />
+        </div>
+        <div className="sweetImageContainer">
+          <div className="sweetImageDetails">
+            <h2>Blueberry Muffins</h2>
+            <p>Deliciously moist muffins that are sure to impress</p>
+          </div>
+          <img src={SweetBlueBerryMuffins} alt="Sweet Blueberry Muffins" />
+        </div>
+        <div className="sweetImageContainer">
+          <div className="sweetImageDetails">
+            <h2>Yellow Cake</h2>
+            <p>Freshly-baked cake sprinkled with powdered sugar</p>
+          </div>
+          <img src={SweetYellowCake} alt="Sweet Yellow Cake" />
+        </div>
+      </div>
     </div>
   );
 }
