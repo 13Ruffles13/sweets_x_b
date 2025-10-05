7d9a057c537a15c962cb4a500d34e1c9a1409504
13Ruffles13 <87215494+13Ruffles13@users.noreply.github.com>
2025-10-04 22:07:36 -0700

    Compiler/Back-end
    Enabled running/debugging tsc to run ts files. Back-end stages created
    to begin crafting interactions with web app.

---
+++ src/Back-end/API/Api.ts
@@ -0,0 +1,15 @@
+// Making API calls to the back-end service
+import SweetData from "../SweetData/SweetData";
+
+console.log(SweetData);
+
+interface SweetProduct {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+}
+
+// async function getPrices(): Promise<SweetProduct> {}
+
+// export default getPrices;
