2096442a9d639ea1f93d21df07a124a1cbd7d03f
13Ruffles13 <87215494+13Ruffles13@users.noreply.github.com>
2025-12-01 18:01:27 -0800

    test: add route.test.tsx
    - RoutePaths component testing
    - test homepage route renders
    - test customer orders route renders

---
+++ src/Routes/route.test.tsx
@@ -0,0 +1,30 @@
+import { MemoryRouter } from "react-router";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
+import RoutePaths from "./routes";
+
+describe("RoutePaths component", () => {
+  test("renders homepage route", () => {
+    // Arrange
+
+    // Act
+    render(
+      <MemoryRouter initialEntries={["/sweets_x_b"]}>
+        <RoutePaths />
+      </MemoryRouter>,
+    );
+
+    // Assert
+    expect(screen.getByText(/home/i)).toBeDefined();
+  });
+
+  test("renders customer orders route", () => {
+    // Arrange
+    window.location.hash = "#/orders";
+    // Act
+    render(<RoutePaths />);
+
+    // Assert
+    expect(screen.getByText(/customerorders/i)).toBeDefined();
+  });
+});
