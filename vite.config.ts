68a74893e53feb62e728804d554abbe7e456fb04
13Ruffles13 <87215494+13Ruffles13@users.noreply.github.com>
2025-12-13 15:22:34 -0800

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sweets_x_b/',
+  base: '/',
   // @ts-expect-error: Vitest=specific config not recognized by Vite types
   test: { environment: 'jsdom',
     setupFiles: './src/Test/setupTests.ts',
