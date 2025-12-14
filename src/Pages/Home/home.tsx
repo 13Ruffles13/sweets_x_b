54dc92450ef99686ef19f861c0f4658b3fffcc6e
13Ruffles13 <87215494+13Ruffles13@users.noreply.github.com>
2025-12-13 17:22:58 -0800

    build(deps): update package-lock.json, home.css and home.tsx

--- src/Pages/Home/home.tsx
+++ src/Pages/Home/home.tsx
@@ -1,16 +1,10 @@
 import sweetsLogoTransparent from "../../Visuals/sweet-logos/Sweets_x_B_logo.jpg";
-import breadBackground from "../../Visuals/backgrounds/bread-1281053_1920.jpg";
 import "./home.css";
 
 function Home() {
   return (
     <>
       <div className="homeContainer">
-        <img
-          id="breadBackground"
-          src={breadBackground}
-          alt="sweetsxb home page background"
-        />
         <div className="sweetsLogoContainer">
           <img
             id="sweetsxb_logo"
